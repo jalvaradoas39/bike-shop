df3ed89e99e38ceb2590b393fbfbaa6e103c5642
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-06-27 18:05:13 -0400

    add properties and constructor

--- private/classes/bicycle.class.php
+++ private/classes/bicycle.class.php
@@ -2,8 +2,35 @@
 
 class Bicycle {
 
+	// properties
+	public $brand;
+	public $model;
+	public $year;
+	public $category;
+	public $color;
+	public $description;
+	public $gender;
+	public $price;
+	protected $weight_kg;
+	protected $condition_id;
 
 
+	// construct
+	public function __construct($args=[]) {
+		$this->brand = $args['brand'] ?? '';
+		$this->model = $args['model'] ?? '';
+		$this->year = $args['year'] ?? '';
+		$this->category = $args['category'] ?? '';
+		$this->color = $args['color'] ?? '';
+		$this->description = $args['description'] ?? '';
+		$this->gender = $args['gender'] ?? '';
+		$this->price = $args['price'] ?? 0;
+		$this->weight_kg = $args['weight_kg'] ?? 0.0;
+		$this->condition_id = $args['condition_id'] ?? 2;
+	}
+
+
+	// methods
 
 }
 
